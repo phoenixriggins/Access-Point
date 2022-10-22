cda545b105798abf1947af2f93fef68252f74324
phoenixriggins <106778186+phoenixriggins@users.noreply.github.com>
2022-10-21 21:09:05 -0500

    add additional examples

--- syntax.py
+++ syntax.py
@@ -1,2 +1,26 @@
+#EXAMPLE 1
 if 5 > 2:
   print("Five is greater than two!")
+
+#EXAMPLE 2
+if 5 > 2:
+print("Five is greater than two!")
+
+#EXAMPLE 3
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+#EXAMPLE 4
+if 5 > 2:
+ print("Five is greater than two!")
+        print("Five is greater than two!")
+
+#EXAMPLE 5
+x = 5
+y = "Hello, World!"
+
+#EXAMPLE 6
+#This is a comment
+print("Hello, Wolrd!")
