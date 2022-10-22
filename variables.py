d6431e5c68925d1dcbe492f52967fd77e78b2d80
phoenixriggins <106778186+phoenixriggins@users.noreply.github.com>
2022-10-21 21:28:55 -0500

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,32 @@
+#EXAMPLE 1
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#EXAMPLE 2
+x = 4     
+x = "Sally" 
+print(x)
+
+#EXAMPLE 3
+x = str(3)  
+y = int(3)   
+z = float(3)
+#print(x, y, z)
+
+#EXAMPLE 4
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+#EXAMPLE 5
+x = "John"
+# is the same as
+x = 'John'
+
+#EXAMPLE 6
+a = 4
+A = "Sally"
+#A will not overwrite a
