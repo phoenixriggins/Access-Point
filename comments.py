8041e7f59cbbb10a8f6ec9668fbf632a3bfd521c
phoenixriggins <106778186+phoenixriggins@users.noreply.github.com>
2022-10-21 21:23:04 -0500

    Create comments.py

---
+++ comments.py
@@ -0,0 +1,24 @@
+#EXAMPLE 1
+#This is a comment
+print("Hello, World!")
+
+#EXAMPLE 2
+print("Hello, World!") #This is a comment
+
+#EXAMPLE 3
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+#EXAMPLE 4
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+#EXAMPLE 5
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
