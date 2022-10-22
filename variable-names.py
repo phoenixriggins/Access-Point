72537f2fe8a65ad2423a23ef71d165eed6415a95
phoenixriggins <106778186+phoenixriggins@users.noreply.github.com>
2022-10-21 21:35:44 -0500

    Create variable-names.py

---
+++ variable-names.py
@@ -0,0 +1,26 @@
+#EXAMPLE 1
+#legal var names
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+#EXAMPLE 2
+#illegal var names
+2myvar = "John"
+my-var = "John"
+my var = "John"
+
+#EXAMPLE 3
+# camel case
+myVariableName = "John"
+
+#EXAMPLE 4
+# pascal case
+MyVariableName = "John"
+
+#EXAMPLE 5
+# snake case
+my_variable_name = "John"
