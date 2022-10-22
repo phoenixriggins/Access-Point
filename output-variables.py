f0b4e04a8bb0e600d168385522db21abb4402529
phoenixriggins <106778186+phoenixriggins@users.noreply.github.com>
2022-10-21 21:44:31 -0500

    Create output-variables.py

---
+++ output-variables.py
@@ -0,0 +1,31 @@
+#Example 1
+x = "Python is awesome"
+print(x)
+
+#Example 2
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+#Example 3
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+#Example 4
+# works as math operator for #s
+x = 5
+y = 10
+print(x + y)
+
+#Example 5
+x = 5
+y = "John"
+print(x + y)
+
+#Example 6
+x = 5
+y = "John"
+print(x, y)
