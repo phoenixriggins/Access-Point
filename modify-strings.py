eb1227b53d961dd5750c493a3347faf7e4aa5407
phoenixriggins <106778186+phoenixriggins@users.noreply.github.com>
2022-10-23 17:18:13 -0500

    Create modify-strings.py

---
+++ modify-strings.py
@@ -0,0 +1,19 @@
+#Example 1
+a = "Hello, World!"
+print(a.upper())
+
+#Example 2
+a = "Hello, World!"
+print(a.lower())
+
+#Example 3
+a = " Hello, World! "
+print(a.strip())
+
+#Example 4
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+#Example 5
+a = "Hello, World!"
+print(a.split(",")) 
